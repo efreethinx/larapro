a800f65a2c738f4079ec8c94f84c33baafeac9d1
Dede Iyan Supardi <efreethinx@gmail.com>
2022-03-30 05:57:36 +0700

    v000

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="twitter:efreethinx" content="Nothing is true, eFreeThinx is permitted">
+    <title>LaraPro</title>
+</head>
+
+<body>
+    <h1 align="center">Welcome</h1>
+</body>
+
+</html>
